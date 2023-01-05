2dc5372347c5eb1b19fd95d38e678dd7b2e5505b
vivianne <viviannjoroge91@gmail.com>
2023-01-05 12:12:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
 function isPalindrome(word) {
   // Write your algorithm here
-  const wordString = word.split('');
-  const wordReverse= wordReverse.join('')
-  const wordJoin = wordReverse.join('')
 
-  if(word === wordJoib){
+  if(word.length < 2) return true
+
+  let start = 0
+  let end = word.length - 1
+
+  while(start <= end){
+    if(word[start] != word[end]){
+      return false
+    }
+    start++
+    end--
+  }
+
     return true
-  }
-  else{
-    return false
-  }
-}
+  
 
+}
 
 /* 
   Add your pseudocode here
-  Declare the variable taking one argument
-  Convert to an array
-  Reverse the array
-  Change it back to a string
-  Compare the original string and reversed string
-  Return solution
+  -loop through the string
+  -check if last element != element at beginning
+  -if elements do not match return false
+  -else return true
+  -increment start
+  -decrement end
 */
 
 /*
   Add written explanation of your solution here
-   declare isPalindrome variable that takes string as an argument
-  change the string into an array using split('')
-  Reverse the array using reverse()
-  change the array back to srtring using join('')
-  use if statement to determine if the original string and the reversed string are equal using ===
+  -A palindrome is a word that is pronunced the same oth ways. In my approach I have
+  created a function that allows a user to input a word. I start by checking if the length 
+  of the word is less than 2. A one letter word and an empty string are palindromes. I therefore 
+  return true if the word has a length of lesss than 2. I loop throug the string once if the length 
+  is greater or equal to 2. This happens in O(n) time and I do not require any extra space
+  to run the function. I return false when the last element does not match the first element.
 */
 
 // You can run `node index.js` to view these console logs
@@ -45,3 +52,5 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
+  
